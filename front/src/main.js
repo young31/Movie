06cdd496cf2ae8e4a21106d74dff5e9003f51db1
young31 <migael38317@gmail.com>
2019-11-25 17:55:15 +0900

    front

--- front/src/main.js
+++ front/src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import SequentialEntrance from 'vue-sequential-entrance'
+import 'vue-sequential-entrance/vue-sequential-entrance.css'
+import VueAnimated from '@codekraft-studio/vue-animated'
+import VueAnime from 'vue-animejs';
+// import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"
 
+Vue.use(VueAnime)
+Vue.use(VueAnimated)
+Vue.use(SequentialEntrance)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
